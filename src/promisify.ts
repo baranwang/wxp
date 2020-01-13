2e2b17f526a5ad71c4b8229f9fa16c0f9f94e18e
Baran <me@baran.wang>
2020-01-13 11:24:38 +0800

    fix: 升级后 ts 报错 Type 'any' is not assignable to type 'never'.

--- src/promisify.ts
+++ src/promisify.ts
@@ -1,4 +1,4 @@
-export default (func: WechatMiniprogram.Wx[keyof WechatMiniprogram.Wx]): any => {
+export default (func: any): any => {
   if (typeof func !== 'function') return func
   return (option: any = {}, component?: any) => new Promise((success, fail) => {
     func({ ...option, success, fail }, component)
